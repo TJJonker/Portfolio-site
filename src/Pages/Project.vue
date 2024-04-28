323cb7f95904292224cc06e1040bf17ce493f517
Tom Jonker <tjj.jonker@gmail.com>
2024-04-28 16:49:57 +0200

    Text sizing test

--- src/Pages/Project.vue
+++ src/Pages/Project.vue
@@ -1,10 +1,15 @@
 <template>
 <div class="con">
-    <router-link :to="'Home'">
-        <img class="me-2" src="ArrowProjects.svg"/>
+    <router-link style="text-decoration: none;" :to="'Home'">
+        <p class="link-text">< Projects</p>
     </router-link>
-    <img class="banner" src="Void_Banner.png" />
 
+    <img class="banner" src="VOID_Banner.png" />
+    
+    <div class="category-container">
+        <p v-for="category in categories" class="category">{{ category }}</p>
+    </div>
+    
     <project-markdown></project-markdown>
 </div>
 </template>
@@ -15,6 +20,13 @@ import ProjectMarkdown from '@/Components/ProjectMarkdown.vue';
 export default {
     components: {
         ProjectMarkdown
+    },
+    data() {
+        return {
+            categories: [
+                "Engine", "Rendering", "ECS", "Physics"
+            ]
+        }
     }
 }
 </script>
@@ -22,5 +34,25 @@ export default {
 <style scoped>
 .banner {
     width: 100%;
+}
+
+.link-text {
+    color: black;
+}
+
+.category-container {
+    display: flex;
+    /* justify-content: flex-start; */
+}
+
+.category {
+    background-color: #172C31;
+    color: #FFF;
+    border-radius: 20px;
+    padding: 4px 8px;
+    margin-right: 4px;
+    margin-top: 8px;
+    margin-bottom: 12px;
+    font-size: 8px;
 }
 </style>
