24664384f7655912f9ab8d83c87aa52f72afe2b7
Tom Jonker <tjj.jonker@gmail.com>
2024-04-29 13:08:11 +0200

    Potentially fixed scroll persistence issue

--- src/routes.js
+++ src/routes.js
@@ -20,4 +20,10 @@ const router = createRouter({
     ]
 });
 
+router.beforeEach((to, from, next) => {
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+    next();
+  });
+
 export default router;
