fe9d296a55519cf4021a96f65a6b3cbd6363c5af
Tom Jonker <tjj.jonker@gmail.com>
2024-12-11 19:42:21 +0100

    Removed global styling

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,7 @@
     <router-view></router-view>
 </template>
 
-<script>
-export default {
-    components: {
-    }
-}
-</script>
-
 <style>
-
-.primary-text-color {
-    color: black;
-}
-
-.secondary-text-color {
-    color: #C4A484;
-}
-
-.con {
-    padding: 56px 40px;
-}
-
-@media only screen and (min-width: 750px) {
-    .con {
-        padding: 56px 15%;
-    }
-}
-
 ::-webkit-scrollbar {
     width: 12px;
 }
