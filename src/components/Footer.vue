d1e5d08b3bed48dbf9c3f87dcf3a795effd24f29
Tom Jonker <tjj.jonker@gmail.com>
2024-12-29 17:02:45 +0100

    Minor changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="footer-top">
-    <div class="column">
-      <h4 class="title uppercase">
-        about
-      </h4>
-      <h4 class="title uppercase">
-        quick links
-      </h4>
-      <h4 class="title uppercase">
-        recent
-      </h4>
-      <h4 class="title uppercase">
-        socials
-      </h4>
+  <footer>  
+    <div class="footer-top">
+      <div class="column">
+        <h4 class="title uppercase">
+          about
+        </h4>
+        <h4 class="title uppercase">
+          quick links
+        </h4>
+        <h4 class="title uppercase">
+          recent
+        </h4>
+        <h4 class="title uppercase">
+          socials
+        </h4>
+      </div>
     </div>
-  </div>
-
-  <div class="footer-bottom">
-
-  </div>
+    
+    <div class="footer-bottom">
+      
+    </div>
+  </footer>
 </template>
   
   <script>
@@ -28,5 +30,8 @@
   </script>
   
 <style lang="css" scoped>
-
+footer {
+  background-color: var(--background-footer-color);
+  border-top: 1px solid var(--background-tertiary-color);
+}
 </style>
