9f90f72da840b629357e643c1f50cb86df552843
Tom Jonker <tjj.jonker@gmail.com>
2025-02-21 15:03:49 +0100

    Fixed animatedTitle scroll animation

--- components/AnimatedTitle.vue
+++ components/AnimatedTitle.vue
@@ -1,68 +1,73 @@
 <template>
-    <div class="text">
-        <h1 class="title" :class="{'animate': isVisible}">{{ title }}</h1>
-        <p class="subheader">{{ subTitle }}</p>
-    </div>
+  <div class="text" ref="animatedtxt">
+    <h1 class="title" :class="{ 'animate': isVisible }">{{ title }}</h1>
+    <p class="subheader">{{ subTitle }}</p>
+  </div>
 </template>
 
-<script>
+<script setup>
 
-export default {
-  name: "AnimatedTitle",
-  props: {
-    title: { type: String, required: true },
-    subTitle: { type: String, required: true },
-  },
-  data() {
-    return {
-      isVisible: false,
-    };
-  },
-  mounted () {
-    window.addEventListener('scroll', this.handleScroll);
-    this.handleAnimation();
-  },
-  unmounted () {
-    window.removeEventListener('scroll', this.handleScroll);
-  },
-  methods: {
-    handleScroll(event) {
-      this.handleAnimation();
-    },
+import { defineProps } from 'vue';
+import { onMounted, ref } from 'vue';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-    handleAnimation() {
-      const rect = this.$el.getBoundingClientRect();
-      this.isVisible = (rect.top < window.innerHeight * getComputedStyle(document.documentElement).getPropertyValue("--animation-scroll-trigger").trim());
-    }
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  subTitle: {
+    type: String,
+    required: true
   }
-}
+});
+
+const animatedtxt = ref(null);
+
+onMounted(() => {
+  if (import.meta.client) {
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  const titleText = gsap.timeline({
+    scrollTrigger: {
+      trigger: animatedtxt.value,
+      start: "top 70%", // Triggers when the section is halfway in view
+      end: "top 30%",
+      scrub: true,
+    }
+  });
+
+  titleText.to(animatedtxt.value.querySelector(".title"), {
+    "--title-bar-width": "100%",
+    duration: 15,
+    ease: "power2.out"
+  }, 0);
+});
 </script>
 
 <style lang="css" scoped>
 .text {
-    margin-bottom: var(--space-l);
+  margin-bottom: var(--space-l);
 }
 
 .title {
-    position: relative;
-    display: inline-block;
-    z-index: 1; 
+  --title-bar-width: 0%;
+  position: relative;
+  display: inline-block;
+  z-index: 1;
 }
 
 .title::before {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    height: 30%;
-    width: 0;
-    background-color: var(--accent-color-underline);
-    z-index: -1;
-    transition: width var(--animation-mode-titles), background-color var(--animation-mode-titles);
-}
-
-
-.title.animate::before {
-    width: 100%; 
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 30%;
+  width: var(--title-bar-width);
+  background-color: var(--accent-color-underline);
+  z-index: -1;
+  transition: background-color var(--animation-mode-titles);
 }
 </style>
