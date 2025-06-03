03993319e12a6fcadcfc2c14e85b0228dd0de762
Tom Jonker <tjj.jonker@gmail.com>
2025-06-03 16:49:43 +0200

    Added the articles page

--- middleware/redirect-me.ts
+++ middleware/redirect-me.ts
@@ -1,4 +1,4 @@
 export default defineNuxtRouteMiddleware(() => {
-    return '/project'
+    return '/articles'
   })
   
---
+++ pages/articles.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="grid-container">
+        <div class="wide articleStack">
+            <AnimatedTitle class="full" title="Articles"
+                subTitle="Exploring idea, sharing knowledge, and documenting my journey through code and design." />
+            <ArticleCard class="full" title="Structural Frameworks and Optimization Techniques in Rendering Engines"
+                subTitle="A look into rendering frameworks and some other stuff that applies to the fields of the creation of rendering engines."
+                date="May 17, 2024" url="/" image="/Images/Void Banner.png" />
+            <ArticleCard class="full" title="Building Seamless Communication: Design of a Real-Time Chat System"
+                subTitle="Exploring the architecture, technologies, and challenges behind creating a fast, reliable, and user-friendly chat application."
+                date="March 14, 2023" url="/" image="/Images/Chatroom Banner.png" />
+            <ArticleCard class="full" title="Crafting Immersive Media Experiences: Development of a Custom Media Player"
+                subTitle="Delving into the core featues, playback optimizations, and user-centric design of a robust media player application."
+                date="November 26, 2022" url="/" image="/Images/MediaPlayer Banner.png" />
+            <ArticleCard class="full" title="Structural Frameworks and Optimization Techniques in Rendering Engines"
+                subTitle="A look into rendering frameworks and some other stuff that applies to the fields of the creation of rendering engines."
+                date="May 17, 2024" url="/" image="/Images/Void Banner.png" />
+            <ArticleCard class="full" title="Building Seamless Communication: Design of a Real-Time Chat System"
+                subTitle="Exploring the architecture, technologies, and challenges behind creating a fast, reliable, and user-friendly chat application."
+                date="March 14, 2023" url="/" image="/Images/Chatroom Banner.png" />
+            <ArticleCard class="full" title="Crafting Immersive Media Experiences: Development of a Custom Media Player"
+                subTitle="Delving into the core featues, playback optimizations, and user-centric design of a robust media player application."
+                date="November 26, 2022" url="/" image="/Images/MediaPlayer Banner.png" last="true" />
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useWindowSize } from '@vueuse/core'
+
+const { width } = useWindowSize()
+</script>
+
+<style lang="scss" scoped>
+.articleStack {
+    display: grid;
+    column-gap: var(--column-gap);
+    row-gap: var(--row-gap);
+    grid-template-columns: repeat(var(--grid-columns-mt), 1fr);
+
+    @media (max-width: $breakpoint-mobile) {
+        row-gap: 0;
+    }
+}
+
+.btn {
+    grid-column: 1 / span 2;
+}
+
+.full {
+    grid-column: 1 / span var(--grid-columns-mt);
+}
+
+.wide {
+    grid-column: 3 / span 8;
+
+    @media (max-width: $breakpoint-desktop) {
+        grid-column: 2 / span 8;
+    }
+
+    @media (max-width: $breakpoint-laptop) {
+        grid-column: 2 / span 6;
+    }
+
+    @media (max-width: $breakpoint-tablet) {
+        grid-column: 1 / span 6;
+    }
+
+}
+</style>
